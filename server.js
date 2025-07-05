d327fb8b4e3b78326e048382628e61bcbd1f9a17
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-07-05 14:35:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const categoryRoutes = require('./routes/Category');
 const productRoutes = require('./routes/products');
-const ordersRoutes = require('./routes/oders');
+const ordersRoutes = require('./routes/orders');
 const app = express();
 
 app.use(cors());
