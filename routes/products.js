e43887a94f99aa1ef0be4114ac25de0c1aa64739
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-07-06 13:14:20 +0300

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Product from "../models/Product";
+import Product from "../models/Product.js";
 import { v2: cloudinary } from "cloudinary";
 
 const router = express.Router();
