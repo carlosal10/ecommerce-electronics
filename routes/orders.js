e4083c53f3650c133888770012990bf4b62d3971
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-07-06 13:13:54 +0300

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,7 @@
 import express from 'express';
-const router = express.Router();
-import Order from '../models/Order';
+import Order from '../models/Order.js';
 import { authenticate } from "../middleware/auth.js";
+const router = express.Router();
 
 router.post("/", authenticate, async (req, res) => {
   const { items, totalItems, totalCost } = req.body;
