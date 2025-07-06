23c4919d5821dd6f19dfa0569dd218ce77d8dbdf
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-07-06 15:07:00 +0300

    Update categories.js

--- models/categories.js
+++ models/categories.js
@@ -4,4 +4,4 @@ const categorySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true }
 });
 
-module.exports = mongoose.model('Category', categorySchema);
+export default mongoose.model('Category', categorySchema);
