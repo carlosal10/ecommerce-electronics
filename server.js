dbddd357fb48f67a27230bfd10b7705b8607ad73
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-07-06 15:30:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -27,29 +27,3 @@ mongoose.connect(process.env.MONGO_URI)
     });
   })
   .catch(err => console.error('❌ MongoDB connection error:', err));
-require('dotenv').config();
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import categoryRoutes from './routes/Category.js';
-import productRoutes from './routes/products.js';
-import ordersRoutes from './routes/orders.js';
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/products', productRoutes); 
-app.use('/api/categories', categoryRoutes);
-app.use("/uploads", express.static("uploads")); // Serve static images
-app.use("/api/orders", ordersRoutes);
-
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('Connected to MongoDB Atlas');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  })
-  .catch(err => console.error(err));
