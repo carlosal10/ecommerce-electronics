b35bf40169e1b5e8d4b196048081e3d2ae88efbc
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-07-15 15:41:42 +0000

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Footer from './components/Footer';
 
 import Login from './pages/Login';
-
 import AdminDashboard from './pages/AdminDashboard';
 import AdminOverview from './pages/AdminOverview';
 import AdminOrders from './pages/AdminOrders';
@@ -15,12 +14,12 @@ import AdminEntryPage from './pages/AdminEntryPage';
 const App = () => (
   <>
     <Routes>
-      {/* Default route */}
+      {/* Public Route */}
       <Route path="/login" element={<Login />} />
 
-      {/* Admin Routes under Dashboard */}
-      <Route path="/" element={<AdminDashboard />}>
-        <Route index element={<AdminOverview />} />
+      {/* Admin Routes under /admin */}
+      <Route path="/admin" element={<AdminDashboard />}>
+        <Route index element={<Navigate to="overview" replace />} />
         <Route path="overview" element={<AdminOverview />} />
         <Route path="orders" element={<AdminOrders />} />
         <Route path="products" element={<AdminProducts />} />
@@ -28,8 +27,8 @@ const App = () => (
         <Route path="data-entry" element={<AdminEntryPage />} />
       </Route>
 
-      {/* Catch-all */}
-      <Route path="*" element={<Navigate to="/" replace />} />
+      {/* Catch-all redirects to admin */}
+      <Route path="*" element={<Navigate to="/admin" replace />} />
     </Routes>
 
     <Footer />
